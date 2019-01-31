d74378ea1d9fc244b2aa1f26e90c4eb7e5baaac2
bluetegu <ronc.ntear@gmail.com>
2019-01-31 10:25:51 -0500

    First version

---
+++ components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logo">
+    <img src="/icon.png"/>
+  </div>
+</template>
+
+<style>
+.logo {
+  display: inline-block;
+  animation: turn 2s linear forwards 1s;
+  transform: rotateX(180deg);
+  position: relative;
+  overflow: hidden;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+</style>
